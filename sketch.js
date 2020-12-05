ee11d22e95280db3162334f38740ba15e333f7a3
KhushbooRajput777 <ksr.oisk@gmail.com>
2020-12-05 21:57:22 +0530

    I have created box class and also I have renamed drop zone to box1, box2 and box 3 also changed the value

--- sketch.js
+++ sketch.js
@@ -1,86 +1,93 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+//define global variables - helicopter, package, ground
+var helicopterIMG, helicopterSprite;
+var packageIMG,packageSprite;
+var packageBody,ground;
+var box1,box2,box3;
+
+//define constants - engine, world, bodies
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var dropzoneside1, dropzoneside2, dropzoneside3;
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+function preload() {
+
+	//load helicopter image
+	helicopterIMG = loadImage("helicopter.png");
+
+	//load package image
+	packageIMG = loadImage("package.png");
+
 }
 
 function setup() {
+
+	//create the canvas
 	createCanvas(800, 700);
+
+	//change the rect mode to center
 	rectMode(CENTER);
 
+	//define, scale, and add an image to the package sprite
+	packageSprite = createSprite(width/2, 80, 10, 10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale = 0.15;
 
-	dropzoneside1 = createSprite(400,650,200,20);
-	dropzoneside1.shapeColor = "red";
-	
-	dropzoneside2 = createSprite(500,610,20,100);
-	dropzoneside2.shapeColor = "red";
+	//define, scale, and add an image to the helicopter sprite
+	helicopterSprite = createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale = 0.6;
 
-	dropzoneside3 = createSprite(300,610,20,100);
-	dropzoneside3.shapeColor = "red";
+	//define and color the ground sprite
+	groundSprite = createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor = color(255);
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
+	//essential lines to create angine and add the world to the engine
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, packageBody);
-	
-	dropzoneside1  = Bodies.rectangle(400,600,200,20, {isStatic:true});
-	World.add(world,dropzoneside1);
-	
-	dropzoneside2 = Bodies.rectangle(500,610,20,100, {isStatic: true});
-	World.add(world,dropzoneside2);
+	//define package body and add it to the world
+	//packageBody radius can change to effect how it collides with the ground
+	packageBody = Bodies.circle(width/2,200,20,{restitution : 0.5, isStatic : true});
+	World.add(world,packageBody);
 
-	dropzoneside3 = Bodies.rectangle(300,610,20,100, {isStatic: true});
-	World.add(world, dropzoneside3);
+	//define ground and add it to the world
+	ground = Bodies.rectangle(width/2,650,width,15,{isStatic : true});
+ 	World.add(world,ground);
 
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	//create the boxes
+	box1 = new Box(400,650,200,20);
+	box2 = new Box(310,610,20,100);
+	box3 = new Box(490,610,20,100);
 
-
-	
+	//update "Engine" with the "engine"
 	Engine.run(engine);
-  
+
 }
-
-
 function draw() {
-  rectMode(CENTER);
+
+  //set background color to black (zero)
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
+  //make position of package sprite to match the package body
+  packageSprite.x = packageBody.position.x;
+  packageSprite.y = packageBody.position.y;
+
+  //call keyPressed() function
   keyPressed();
-}
 
+  //display boxes
+  box1.display();
+  box2.display();
+  box3.display();
+
+  //function to draw sprites
+  drawSprites();
+}
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-	Matter.Body.setStatic(packageBody,false);
-  }
-}
 
+	//if the down arrow is pressed, the static of the package body becomes false
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody, false);
+	}
 
-
-	
-
-
+} 
